b01d9b06130484270b8bf7d63f9bfeedb8fe1819
Tim van Dijen <tvdijen@gmail.com>
2023-03-20 23:18:16 +0100

    Fix test-suite

--- src/Controller/Autotest.php
+++ src/Controller/Autotest.php
@@ -8,7 +8,6 @@ use SimpleSAML\Auth;
 use SimpleSAML\Configuration;
 use SimpleSAML\Error;
 use SimpleSAML\Session;
-use SimpleSAML\Utils;
 use SimpleSAML\XHTML\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
